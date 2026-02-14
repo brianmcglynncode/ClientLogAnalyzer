edebd944fc0fd00c48c26fbaa271d2d47a9f614f
Brian O <brian@example.com>
2026-02-14 17:16:39 +0000

    Update server.js to use process.env.PORT for Railway deployment

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const multer = require('multer');
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 // Setup Storage for Uploads
 const storage = multer.diskStorage({
